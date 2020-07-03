3f9690cc7929bcb914ee1754440fe1aba2ff79a3
Hannes Hinrichs <hannes.hinrichs@senselab.io>
2020-07-03 02:43:50 +0200

    -Removed Main Camera from Prefab

---
+++ Teambuilding_Trust/Assets/Scripts/Normcore/XRRigNetworkable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRRigNetworkable : XRRig
+{
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+
+    }
+    }
+}
