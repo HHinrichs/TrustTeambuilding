aefedebd0338e05a1a88c5f3a7e1e3b4aa20b613
Hannes Hinrichs <hannes.hinrichs@senselab.io>
2020-07-04 17:07:28 +0200

    -Added Hand animation dependencies

---
+++ Teambuilding_Trust/Assets/XRCollection/HandAnimation/Scripts/HandPhysics.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace XRCollection.Hand
+{
+    public class HandPhysics : MonoBehaviour
+    {
+        private void Awake()
+        {
+
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void SetTargetPosition()
+        {
+
+        }
+
+        private void SetTargetRotation()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+
+        }
+
+        private void MoveToController()
+        {
+
+        }
+
+        private void RotateToController()
+        {
+
+        }
+
+        public void TeleportToTarget()
+        {
+
+        }
+    }
+}
