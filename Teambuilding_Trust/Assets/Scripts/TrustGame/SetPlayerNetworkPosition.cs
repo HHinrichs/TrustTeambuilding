909f43ed62490c5507bc10e1d73b556172acfed7
Hannes Hinrichs <hannes.hinrichs@senselab.io>
2020-07-30 00:42:27 +0200

    -Fixed bug that you didnt waited till connected

--- Teambuilding_Trust/Assets/Scripts/TrustGame/SetPlayerNetworkPosition.cs
+++ Teambuilding_Trust/Assets/Scripts/TrustGame/SetPlayerNetworkPosition.cs
@@ -23,54 +23,77 @@ public class SetPlayerNetworkPosition : MonoBehaviour
 
     public void ChooseMyPositionAtConnection(Realtime realtime)
     {
-
-        if (gameManager.isClient && gameManager.GetNetworkPlayerPositionsInitialized)
-        {
-            StartCoroutine(ChooseMyPositionCoroutine());
-        }
-        else
-        {
-            Debug.Log("It seems that you are the SERVER OR the server has not defined network positions yet... therefore there is no server availible!");
-            Debug.Log("gameManager.isClient == " + gameManager.isClient + "///gameManager.GetNetworkPlayerPositionsInitialized " + gameManager.GetNetworkPlayerPositionsInitialized);
-        }
+        StartCoroutine(ChooseMyPositionAtConnectionCoroutine());
     }
-
     public void ChooseMyPositionAtReset()
     {
+        StartCoroutine(ChooseMyPositionAtResetCoroutine());
+    }
+
+    IEnumerator ChooseMyPositionAtConnectionCoroutine()
+    {
+        yield return new WaitUntil(() => realtime.connected);
 
         if (gameManager.isClient && gameManager.GetNetworkPlayerPositionsInitialized)
         {
-            StartCoroutine(ChooseMyPositionCoroutine());
+            playerNetworkPositionInt = gameManager.NetworkPlayerPositions.GetIntValue;
+
+            for (int i = 0; i < gameManager.GetNumbersOfParticipatingPlayers; ++i)
+            {
+                if (IntToBoolean.IsBitSetTo1(playerNetworkPositionInt, 0))
+                {
+                    playerRig.gameObject.transform.position = PlayerNetworkPositions[i].position;
+                    playerRig.gameObject.transform.rotation = PlayerNetworkPositions[i].rotation;
+                    playerNetworkPositionInt = IntToBoolean.SetBitTo0(playerNetworkPositionInt, i);
+                    // SETS THE UPDATED PLAYER POSITION VALUE FOR THIS PLAYER
+                    gameManager.NetworkPlayerPositions.SetIntValue(IntToBoolean.SetBitTo0(playerNetworkPositionInt, i));
+                    Debug.Log("PLAYER POSITION SET TO " + PlayerNetworkPositions[i]);
+                    yield break;
+                }
+            }
+
+            Debug.Log("NO POSITION LEFT, SETTING TO DEFAULT POSITION");
+            playerRig.gameObject.transform.position = PlayerNetworkPositions[3].position;
+            playerRig.gameObject.transform.rotation = PlayerNetworkPositions[3].rotation;
         }
         else
         {
             Debug.Log("It seems that you are the SERVER OR the server has not defined network positions yet... therefore there is no server availible!");
             Debug.Log("gameManager.isClient == " + gameManager.isClient + "///gameManager.GetNetworkPlayerPositionsInitialized " + gameManager.GetNetworkPlayerPositionsInitialized);
         }
+        yield return null;
     }
-
-    public IEnumerator ChooseMyPositionCoroutine()
+    IEnumerator ChooseMyPositionAtResetCoroutine()
     {
-        playerNetworkPositionInt = gameManager.NetworkPlayerPositions.GetIntValue;
-        
-        for(int i = 0; i < gameManager.GetNumbersOfParticipatingPlayers; ++i)
+        yield return new WaitUntil(() => realtime.connected);
+
+        if (gameManager.isClient && gameManager.GetNetworkPlayerPositionsInitialized)
         {
-            if (IntToBoolean.IsBitSetTo1(playerNetworkPositionInt, 0))
+            playerNetworkPositionInt = gameManager.NetworkPlayerPositions.GetIntValue;
+
+            for (int i = 0; i < gameManager.GetNumbersOfParticipatingPlayers; ++i)
             {
-                playerRig.gameObject.transform.position = PlayerNetworkPositions[i].position;
-                playerRig.gameObject.transform.rotation = PlayerNetworkPositions[i].rotation;
-                playerNetworkPositionInt = IntToBoolean.SetBitTo0(playerNetworkPositionInt, i);
-                // SETS THE UPDATED PLAYER POSITION VALUE FOR THIS PLAYER
-                gameManager.NetworkPlayerPositions.SetIntValue(IntToBoolean.SetBitTo0(playerNetworkPositionInt, i));
-                Debug.Log("PLAYER POSITION SET TO " + PlayerNetworkPositions[i]);
-                yield break;
+                if (IntToBoolean.IsBitSetTo1(playerNetworkPositionInt, 0))
+                {
+                    playerRig.gameObject.transform.position = PlayerNetworkPositions[i].position;
+                    playerRig.gameObject.transform.rotation = PlayerNetworkPositions[i].rotation;
+                    playerNetworkPositionInt = IntToBoolean.SetBitTo0(playerNetworkPositionInt, i);
+                    // SETS THE UPDATED PLAYER POSITION VALUE FOR THIS PLAYER
+                    gameManager.NetworkPlayerPositions.SetIntValue(IntToBoolean.SetBitTo0(playerNetworkPositionInt, i));
+                    Debug.Log("PLAYER POSITION SET TO " + PlayerNetworkPositions[i]);
+                    yield break;
+                }
             }
+
+            Debug.Log("NO POSITION LEFT, SETTING TO DEFAULT POSITION");
+            playerRig.gameObject.transform.position = PlayerNetworkPositions[3].position;
+            playerRig.gameObject.transform.rotation = PlayerNetworkPositions[3].rotation;
         }
-
-        Debug.Log("NO POSITION LEFT, SETTING TO DEFAULT POSITION");
-        playerRig.gameObject.transform.position = PlayerNetworkPositions[3].position;
-        playerRig.gameObject.transform.rotation = PlayerNetworkPositions[3].rotation;
-
-
+        else
+        {
+            Debug.Log("It seems that you are the SERVER OR the server has not defined network positions yet... therefore there is no server availible!");
+            Debug.Log("gameManager.isClient == " + gameManager.isClient + "///gameManager.GetNetworkPlayerPositionsInitialized " + gameManager.GetNetworkPlayerPositionsInitialized);
+        }
+        yield return null;
     }
 }
