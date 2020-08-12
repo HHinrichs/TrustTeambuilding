43b65ae6e6cf68acbee5205281afefa7f4cbfede
Hannes Hinrichs <hannes.hinrichs@senselab.io>
2020-08-12 21:54:28 +0200

    Removed crap from PartnerAvatar

---
+++ Teambuilding_Trust/Assets/Scripts/TrustGame/SetLocalAvatarPrefabForPlayers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class SetLocalAvatarPrefabForPlayers : MonoBehaviour
+{
+    public RealtimeAvatarManager ServerAvatarManager;
+    public GameObject NoBodyAvatar;
+    public GameObject BodyAvatar;
+    public void Start()
+    {
+        if (ServerAvatarManager == null)
+            ServerAvatarManager = FindObjectOfType<RealtimeAvatarManager>();
+    }
+
+    public void ChangeAvatarAppearanceToBodyAvatar()
+    {
+        ServerAvatarManager.localAvatarPrefab = BodyAvatar;
+    }
+
+    public void ChangeAvatarAppearanceToNoBodyAvatar()
+    {
+        ServerAvatarManager.localAvatarPrefab = NoBodyAvatar;
+
+    }
+}
