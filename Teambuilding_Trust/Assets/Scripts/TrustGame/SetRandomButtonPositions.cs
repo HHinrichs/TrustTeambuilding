3e12f9ffda5e2fcd55607c1069357d03b1410e75
Hannes Hinrichs <hannes.hinrichs@web.de>
2020-08-20 23:49:07 +0200

    -Changed Button Positions for different Podests

---
+++ Teambuilding_Trust/Assets/Scripts/TrustGame/SetRandomButtonPositions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+using System;
+
+public class SetRandomButtonPositions : MonoBehaviour
+{
+    [SerializeField] List<Transform> Buttons;
+    [SerializeField] List<Transform> ButtonPositions;
+    [SerializeField] List<Material> ButtonMaterials;
+
+    private void Awake()
+    {
+        ShuffleList();
+        //for(int i = 0; i < ButtonPositions.Count; ++i)
+        //{
+        //    GameObject button = Instantiate(ButtonPrefab,ButtonPositions[i].position,ButtonPositions[i].rotation, ButtonPositions[i].transform);
+        //    ButtonLUT buttonLut = button.GetComponent<ButtonLUT>();
+        //    buttonLut.ButtonController.buttonNumber = i;
+        //    buttonLut.SymbolPlaneMeshRenderer.material = ButtonMaterials[i];
+        //}
+
+        for(int i = 0; i < Buttons.Count; ++i)
+        {
+            Buttons[i].transform.position = ButtonPositions[i].transform.position;
+            Buttons[i].transform.rotation = ButtonPositions[i].transform.rotation;
+            Buttons[i].gameObject.SetActive(true);
+        }
+    }
+
+    public void ShuffleList()
+    {
+        for (int i = 0; i < ButtonPositions.Count; i++)
+        {
+            Transform temp = ButtonPositions[i];
+            int randomIndex = UnityEngine.Random.Range(i, ButtonPositions.Count);
+            ButtonPositions[i] = ButtonPositions[randomIndex];
+            ButtonPositions[randomIndex] = temp;
+        }
+    }
+
+}
