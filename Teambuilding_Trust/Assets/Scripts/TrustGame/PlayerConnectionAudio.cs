639aa71b7125120ce5f5bf5a3e4affc1ea145b27
Hannes Hinrichs <hannes.hinrichs@web.de>
2020-08-20 20:23:10 +0200

    -Added Button Click, Added Connection Disconnection Clip, Changed "Start button from Spectator"

---
+++ Teambuilding_Trust/Assets/Scripts/TrustGame/PlayerConnectionAudio.cs
@@ -0,0 +1,46 @@
+﻿using Normal.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConnectionAudio : MonoBehaviour
+{
+    private RealtimeAvatarManager realtimeAvatarManager;
+    private AudioSource audioSource;
+
+    [SerializeField] AudioClip ConnectionClip;
+    [SerializeField] AudioClip DisconnectionClip;
+
+    private void Awake()
+    {
+        realtimeAvatarManager = FindObjectOfType<RealtimeAvatarManager>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Start()
+    {
+        realtimeAvatarManager.avatarCreated += PlayConnectionSound;
+        realtimeAvatarManager.avatarDestroyed += PlayDisconnectionSound;
+    }
+
+    private void OnDestroy()
+    {
+        realtimeAvatarManager.avatarCreated -= PlayConnectionSound;
+        realtimeAvatarManager.avatarDestroyed -= PlayDisconnectionSound;
+    }
+
+    public void PlayConnectionSound(RealtimeAvatarManager avatarManager, RealtimeAvatar avatar, bool isLocalAvatar)
+    {
+        if (isLocalAvatar)
+            return;
+
+        audioSource.PlayOneShot(ConnectionClip);
+    }
+
+    public void PlayDisconnectionSound(RealtimeAvatarManager avatarManager, RealtimeAvatar avatar, bool isLocalAvatar)
+    {
+        if (isLocalAvatar)
+            return;
+
+        audioSource.PlayOneShot(DisconnectionClip);
+    }
+}
