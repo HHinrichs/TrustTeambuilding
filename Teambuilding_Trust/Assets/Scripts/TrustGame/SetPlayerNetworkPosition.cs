16ed96be48d284cceaa37cfde383f7c108ebbf52
Hannes Hinrichs <hannes.hinrichs@web.de>
2020-08-26 23:37:22 +0200

    -Updated Network positioning

--- Teambuilding_Trust/Assets/Scripts/TrustGame/SetPlayerNetworkPosition.cs
+++ Teambuilding_Trust/Assets/Scripts/TrustGame/SetPlayerNetworkPosition.cs
@@ -33,15 +33,21 @@ public class SetPlayerNetworkPosition : MonoBehaviour
 
     IEnumerator ChooseMyPositionAtConnectionCoroutine()
     {
-        yield return new WaitUntil(() => realtime.connected);
+        //yield return new WaitUntil(() => realtime.connected);
 
         if (gameManager.isClient)
         {
+            while (!gameManager.NetworkPlayerPositions.realtimeView.isOwnedLocally)
+            {
+                gameManager.NetworkPlayerPositions.realtimeView.RequestOwnership();
+                Debug.Log(gameManager.NetworkPlayerPositions.realtimeView.isOwnedLocally);
+                yield return new WaitForSeconds(0.05f);
+            }
             playerNetworkPositionInt = gameManager.NetworkPlayerPositions.GetIntValue;
 
             for (int i = 0; i < gameManager.GetNumbersOfParticipatingPlayers; ++i)
             {
-                if (IntToBoolean.IsBitSetTo1(playerNetworkPositionInt, 0))
+                if (IntToBoolean.IsBitSetTo1(playerNetworkPositionInt, i))
                 {
                     playerRig.gameObject.transform.position = PlayerNetworkPositions[i].position;
                     playerRig.gameObject.transform.rotation = PlayerNetworkPositions[i].rotation;
@@ -52,6 +58,7 @@ public class SetPlayerNetworkPosition : MonoBehaviour
                     // SETS THE UPDATED PLAYER POSITION VALUE FOR THIS PLAYER
                     gameManager.NetworkPlayerPositions.SetIntValue(IntToBoolean.SetBitTo0(playerNetworkPositionInt, i));
                     Debug.Log("PLAYER POSITION SET TO " + PlayerNetworkPositions[i]);
+                    gameManager.NetworkPlayerPositions.realtimeView.ClearOwnership();
                     yield break;
                 }
             }
@@ -59,6 +66,7 @@ public class SetPlayerNetworkPosition : MonoBehaviour
             Debug.Log("NO POSITION LEFT, SETTING TO DEFAULT POSITION");
             playerRig.gameObject.transform.position = PlayerNetworkPositions[3].position;
             playerRig.gameObject.transform.rotation = PlayerNetworkPositions[3].rotation;
+            gameManager.NetworkPlayerPositions.realtimeView.ClearOwnership();
         }
         else
         {
@@ -73,11 +81,16 @@ public class SetPlayerNetworkPosition : MonoBehaviour
 
         if (gameManager.isClient)
         {
+            while(!gameManager.NetworkPlayerPositions.realtimeView.isOwnedLocally)
+            {
+                gameManager.NetworkPlayerPositions.realtimeView.RequestOwnership();
+                yield return new WaitForSeconds(0.05f);
+            }
             playerNetworkPositionInt = gameManager.NetworkPlayerPositions.GetIntValue;
 
             for (int i = 0; i < gameManager.GetNumbersOfParticipatingPlayers; ++i)
             {
-                if (IntToBoolean.IsBitSetTo1(playerNetworkPositionInt, 0))
+                if (IntToBoolean.IsBitSetTo1(playerNetworkPositionInt, i))
                 {
                     playerRig.gameObject.transform.position = PlayerNetworkPositions[i].position;
                     playerRig.gameObject.transform.rotation = PlayerNetworkPositions[i].rotation;
@@ -87,6 +100,7 @@ public class SetPlayerNetworkPosition : MonoBehaviour
                     // SETS THE UPDATED PLAYER POSITION VALUE FOR THIS PLAYER
                     gameManager.NetworkPlayerPositions.SetIntValue(IntToBoolean.SetBitTo0(playerNetworkPositionInt, i));
                     Debug.Log("PLAYER POSITION SET TO " + PlayerNetworkPositions[i]);
+                    gameManager.NetworkPlayerPositions.realtimeView.ClearOwnership();
                     yield break;
                 }
             }
@@ -94,6 +108,7 @@ public class SetPlayerNetworkPosition : MonoBehaviour
             Debug.Log("NO POSITION LEFT, SETTING TO DEFAULT POSITION");
             playerRig.gameObject.transform.position = PlayerNetworkPositions[3].position;
             playerRig.gameObject.transform.rotation = PlayerNetworkPositions[3].rotation;
+            gameManager.NetworkPlayerPositions.realtimeView.ClearOwnership();
         }
         else
         {
