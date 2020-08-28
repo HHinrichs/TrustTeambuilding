badf7b56491b210edace612cadf9652c09361f33
Hannes Hinrichs <hannes.hinrichs@web.de>
2020-08-28 16:18:42 +0200

    -Added possibility to change playerheight with q and e

---
+++ Teambuilding_Trust/Assets/Scripts/AvatarHeightChangeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarHeightChangeController : MonoBehaviour
+{
+    private Vector3 newTransformPosition;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (transform.position.y > 0)
+            {
+                newTransformPosition = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
+                transform.position = newTransformPosition;
+            }
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            if (transform.position.y < 0.5f)
+            {
+                newTransformPosition = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+                transform.position = newTransformPosition;
+            }
+        }
+    }
+}
